1470abd3302fe51822890c5d6353cbcaab6bd22f
pham tung anh <phamtunganh98@gmail.com>
2018-07-23 16:21:42 +0700

    add DAL folder, DataContext folder, change connectionString

---
+++ FinalProject/DAL/ProductDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+namespace FinalProject.DAL
+{
+    public class ProductDAO: DataContext.DataConnection
+    {
+        // get first 10 items from table Product
+        public List<Models.Product> GetList10Product()
+        {
+            List<Models.Product> list = new List<Models.Product>();
+            SqlDataAdapter da = new SqlDataAdapter();
+            
+            
+        } 
+
+    }
+}

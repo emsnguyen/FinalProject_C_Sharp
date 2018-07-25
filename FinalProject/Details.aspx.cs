183fdf88586447710ac879e20b05d117702cecb6
phonng <heartgold1405@gmail.com>
2018-07-25 15:37:07 +0700

    Front-End with masterpage

--- FinalProject/Details.aspx.cs
+++ FinalProject/Details.aspx.cs
@@ -1,23 +1,23 @@
-﻿using System;
+﻿using FinalProject.DAL;
+using FinalProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using FinalProject.Models;
-using FinalProject.DAL;
-using FinalProject.Models;
 
 namespace FinalProject
 {
-    public partial class singleIndex : System.Web.UI.Page
+    public partial class WebForm2 : System.Web.UI.Page
     {
         ProductDAO productDao = new ProductDAO();
         public Product product;
         protected void Page_Load(object sender, EventArgs e)
-        {           
+        {
             int id = Convert.ToInt32(Request["productID"]);
             product = productDao.GetProductByID(id);
+
         }
     }
 }
--- FinalProject/Master.Master.designer.cs
+++ FinalProject/Master.Master.designer.cs
@@ -22,6 +22,24 @@ namespace FinalProject {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// lblInfo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblInfo;
+        
+        /// <summary>
+        /// loginLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink loginLink;
+        
+        /// <summary>
         /// submit control.
         /// </summary>
         /// <remarks>
